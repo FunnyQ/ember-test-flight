873d89a9fd0d7af167ad57f9fac22d9d4bcde260
FunnyQ <funnyq@funnyq.com>
2017-01-03 04:48:02 +0800

    [update] updating saveInvitation action
    handlebars 的條件判斷式
    
    https://guides.emberjs.com/v2.10.0/templates/conditionals/

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -9,8 +9,15 @@ export default Ember.Controller.extend({
   actualEmailAddress: Ember.computed('emailAddress', function() { 
     console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
   }),
-
   emailAddressChanged: Ember.observer('emailAddress', function() { 
     console.log('observer is called', this.get('emailAddress')); 
-  })
+  }),
+
+  actions: {
+    saveInvitation() {
+      console.log(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `感謝您，您的 Email：${this.get('emailAddress')} 已經加入邀請等候名單！`);
+      this.set('emailAddress', '');
+    }
+  }
 });
