431d4165136848e07f70c306b498d90c8a5242a8
FunnyQ <funnyq@funnyq.com>
2017-06-24 19:19:37 +0800

    :package: move validation to model, using model in route.
     - 與 model 自身相關的 validation 和 computed properties 可以放在 model 裡
     - 盡量使用 routes 來定義 model 和 actions

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -2,27 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  isValidEmail: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-  isMessageEnoughLong: Ember.computed.gte('contactMessage.length', 5),
-  isValid: Ember.computed.and('isValidEmail', 'isMessageEnoughLong'),
-
-  isDisabled: Ember.computed.not('isValid'),
-
-  actions: {
-    sendMessage: function(){
-      var email = this.get('emailAddress');
-      var message = this.get('contactMessage');
-      var responseMessage = `To: ${email}, Message: ${message}`;
-      let newContact = this.store.createRecord('contact', {
-        email: email,
-        message: message
-      })
-
-      newContact.save().then((response) => {
-        this.set('responseMessage', responseMessage);
-        this.set('emailAddress', '');
-        this.set('contactMessage', '');
-      })
-    }
-  }
 });
--- app/models/contact.js
+++ app/models/contact.js
@@ -2,5 +2,13 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   email: DS.attr('string'),
-  message: DS.attr('string')
+  message: DS.attr('string'),
+
+  // validations
+  isValidEmail: Ember.computed.match('email', /^.+@.+\..+$/),
+  isMessageEnoughLong: Ember.computed.gte('message.length', 5),
+
+  // states
+  isValid: Ember.computed.and('isValidEmail', 'isMessageEnoughLong'),
+  isDisabled: Ember.computed.not('isValid')
 });
